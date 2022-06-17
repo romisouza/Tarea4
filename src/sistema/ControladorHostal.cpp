baf7c32d1d807ef1ec7795d68345012f4f06725e
braianmendizab <braianmcapo@gmail.com>
2022-06-16 22:13:34 -0300

    Update ControladorHostal.cpp

--- src/sistema/ControladorHostal.cpp
+++ src/sistema/ControladorHostal.cpp
@@ -268,7 +268,6 @@ set<DTReserva*> ControladorHostal::ObtenerReservasNC(std::string nombreHostal, s
 	Hostal* Hst =ColHostales.find(nombreHostal)->second;
 	this->hostalIngresado = Hst; //para recordar
 	set<DTReserva*> colReservasNC = Hst->BuscarRes(email);
-	set<DTReserva*> colReservasNC;
 
 	return colReservasNC;
 	}
